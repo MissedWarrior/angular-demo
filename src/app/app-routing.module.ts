eba2d3075198e1ac4efceb69bfb8f0c313041e6d
Rasul <gad-ras1@yandex.ru>
2022-10-03 11:35:48 +0300

    Add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '/',
+    redirectTo: '/signup',
+  },
+  {
+    path: '/signup',
+    children: [
+      {
+        path: '',
+        component: undefined,
+        pathMatch: 'full',
+      },
+      {
+        path: 'email-confirm',
+        component: undefined,
+        pathMatch: 'full',
+      },
+      {
+        path: 'success',
+        pathMatch: 'full',
+        component: undefined,
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
